19602508ccf1f36c83cd25af41604009f8b9b928
Aaron <aaron@nanu-c.org>
2023-08-30 00:40:50 +0200

    set manual paths for marker

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,33 +1,99 @@
 import React, { Component } from 'react';
+import Leaflet from 'leaflet';
 import {
-  MapContainer, TileLayer, Marker, Circle, Polyline,
+  MapContainer,
+  TileLayer,
+  Marker,
+  Circle,
+  Polyline,
 } from 'react-leaflet';
+
+Leaflet.Icon.Default.mergeOptions({
+  iconRetinaUrl: '/marker-icon-2x.png',
+  iconUrl: '/marker-icon.png',
+  shadowUrl: '/marker-shadow.png',
+});
 
 export default class MapComponent extends Component {
   render() {
     return (
-      this.props.positions.length > 0
-      && (
-      <MapContainer center={this.props.position} zoom={this.props.viewport.zoom} style={{ height: this.props.height, width: this.props.width }}>
-        <TileLayer
-          url="https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png"
-          subdomains={['maps', 'maps1', 'maps2', 'maps3']}
-          minZoom="1"
-          maxZoom="18"
-        />
-        {this.props.positions.map((pos) => (
-          <Marker position={pos} key={pos} />
-        ))}
-        <Circle center={this.props.viewport.center} radius={3000} fill={false} weight={2} color="#ff7677" />
-        <Circle center={this.props.viewport.center} radius={5000} fill={false} weight={2} color="#ff474b" />
-        <Circle center={this.props.viewport.center} radius={7000} fill={false} weight={2} color="#ff1d23" />
-        <Circle center={this.props.viewport.center} radius={9000} fill={false} weight={2} color="#ff000b" />
-        <Circle center={this.props.viewport.center} radius={11000} fill={false} weight={2} color="#ff000b" />
-        <Circle center={this.props.viewport.center} radius={13000} fill={false} weight={2} color="#db0007" />
-        <Circle center={this.props.viewport.center} radius={15000} fill={false} weight={2} color="#980006" />
-        <Circle center={this.props.viewport.center} radius={17000} fill={false} weight={2} color="#5e0004" />
-        <Polyline positions={this.props.positions} color="#67ff38" opacity={0.5} />
-      </MapContainer>
+      this.props.positions.length > 0 && (
+        <MapContainer
+          center={this.props.position}
+          zoom={this.props.viewport.zoom}
+          style={{ height: this.props.height, width: this.props.width }}
+        >
+          <TileLayer
+            url='https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png'
+            subdomains={['maps', 'maps1', 'maps2', 'maps3']}
+            minZoom='1'
+            maxZoom='18'
+          />
+          {this.props.positions.map((pos) => (
+            <Marker position={pos} key={pos} />
+          ))}
+          <Circle
+            center={this.props.viewport.center}
+            radius={3000}
+            fill={false}
+            weight={2}
+            color='#ff7677'
+          />
+          <Circle
+            center={this.props.viewport.center}
+            radius={5000}
+            fill={false}
+            weight={2}
+            color='#ff474b'
+          />
+          <Circle
+            center={this.props.viewport.center}
+            radius={7000}
+            fill={false}
+            weight={2}
+            color='#ff1d23'
+          />
+          <Circle
+            center={this.props.viewport.center}
+            radius={9000}
+            fill={false}
+            weight={2}
+            color='#ff000b'
+          />
+          <Circle
+            center={this.props.viewport.center}
+            radius={11000}
+            fill={false}
+            weight={2}
+            color='#ff000b'
+          />
+          <Circle
+            center={this.props.viewport.center}
+            radius={13000}
+            fill={false}
+            weight={2}
+            color='#db0007'
+          />
+          <Circle
+            center={this.props.viewport.center}
+            radius={15000}
+            fill={false}
+            weight={2}
+            color='#980006'
+          />
+          <Circle
+            center={this.props.viewport.center}
+            radius={17000}
+            fill={false}
+            weight={2}
+            color='#5e0004'
+          />
+          <Polyline
+            positions={this.props.positions}
+            color='#67ff38'
+            opacity={0.5}
+          />
+        </MapContainer>
       )
     );
   }
